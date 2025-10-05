fd9680811065eea0460479d1eb2718c95102ec89
Awais Hyder Brohi <awaishyderbr@gmail.com>
2025-10-05 09:54:29 -0700

    project created

---
+++ src/main/java/com/projects/chat_websocket/ChatWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.projects.chat_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebsocketApplication.class, args);
+	}
+
+
+}
